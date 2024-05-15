adbef42c9151ae13f64d8183bbb6d8c021a51c80
ValeriaMyronets <valery.mir.11@gmail.com>
2024-05-15 16:48:58 +0300

    additional changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todos_vue_js_demo/',
+  base: '/todos_vue_js/',
   plugins: [
     vue(),
   ],
